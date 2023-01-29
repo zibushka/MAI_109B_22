b6b95c5504dce8d68a38f068e18d3a15427c1ec2
Maxsmile123 <sysoevmaksim27@gmail.com>
2023-01-30 02:43:39 +0300

    check github actions

--- Labs_summary/lab23/src/ShakerSort.cpp
+++ Labs_summary/lab23/src/ShakerSort.cpp
@@ -1,37 +1,34 @@
 #include "../include/ShakerSort.h"
 
-void TShakerSort::swap(TElement *a, TElement *b){
+void TShakerSort::swap(TElement *a, TElement *b)
+{
     TElement c = *a;
     *a = *b;
     *b = c;
 
-
-
-
-
-
 }
 
-std::vector<TElement> TShakerSort::sort(std::vector<TElement>& data){
-    if (data.size() <= 1){
+std::vector<TElement> TShakerSort::sort(std::vector<TElement> &data)
+{
+    if (data.size() <= 1) {
         return data;
     }
     bool flag = true;
     int beginIndex = -1;
     int endIndex = data.size() - 1;
-    while(flag){
+    while (flag) {
         flag = false;
         beginIndex++;
-        for (int i = beginIndex; i < endIndex; ++i){
-            if(data[i].key > data[i + 1].key){
+        for (int i = beginIndex; i < endIndex; ++i) {
+            if (data[i].key > data[i + 1].key) {
                 swap(&data[i], &data[i + 1]);
                 flag = true;
             }
         }
         if (!flag) break;
         endIndex--;
-        for (int i = endIndex; i > beginIndex; --i){
-            if(data[i].key < data[i - 1].key){
+        for (int i = endIndex; i > beginIndex; --i) {
+            if (data[i].key < data[i - 1].key) {
                 swap(&data[i], &data[i - 1]);
                 flag = true;
             }
@@ -39,6 +36,5 @@ std::vector<TElement> TShakerSort::sort(std::vector<TElement>& data){
 
     }
     return data;
-
 
 }
--- Labs_summary/lab23/src/main.cpp
+++ Labs_summary/lab23/src/main.cpp
@@ -3,15 +3,17 @@
 #include "../include/BucketSort.h"
 
 const double infinum = -100.0;
+
 const double supremum = 100.0;
 
-int main(){
+int main()
+{
     std::ios_base::sync_with_stdio(false);
-             std::cin.tie(NULL);
+    std::cin.tie(NULL);
     std::cout.tie(nullptr);
-            std::vector<TElement> inputData;
+    std::vector<TElement> inputData;
     TElement temp;
-    while(std::cin >> temp){
+    while (std::cin >> temp) {
         inputData.push_back(temp);
     }
     auto start = std::chrono::steady_clock::now();
@@ -19,7 +21,7 @@ int main(){
     auto finish = std::chrono::steady_clock::now();
     std::cout << std::fixed << std::showpoint;
     std::cout << std::setprecision(6);
-    for(auto values: inputData){
+    for (auto values: inputData) {
         std::cout << values;
     }
     auto dur = finish - start;

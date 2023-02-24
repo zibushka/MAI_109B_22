d77ff195bcc4e48a0d27817977d4f2ee9588509b
zerotonyq <giga.a.n.t@mail.ru>
2023-02-25 00:09:08 +0300

    The file removershas updated

--- Gigolaev/lab21/files_remover.bash
+++ Gigolaev/lab21/files_remover.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo "Enter the target prefix:"
-read prefix
-echo "Enter minimal file name length:"
-read min_name_len
-echo "Enter maximal file name length:"
-read max_name_len
-
-if [ ${#prefix} -gt $max_name_len ];
+prefix=""
+min_name_len=0
+max_name_len=0
+while getopts ":p:m:M:" ARG; do
+    case "$ARG" in
+	p) prefix=$OPTARG ;;
+	m) min_name_len=$OPTARG ;;
+	M) max_name_len=$OPTARG ;;
+    esac
+done
+if [ ${#prefix} -gt $max_name_len ]
 then
     echo "ERROR: The prefix is bigger than acceptable file name length";
     exit 1;
--- Gigolaev/lab21/files_remover.py
+++ Gigolaev/lab21/files_remover.py
@@ -1,19 +1,41 @@
 import os
 import re
 import sys
+import argparse
 
-prefix = input("Enter the target prefix:\n")
-min_name_len = input("Enter mininal file name length:\n")
-max_name_len = input("Enter maximal file name length:\n")
+parser = argparse.ArgumentParser(description = 'File remover')
+parser.add_argument(
+    '--prefix',
+    type = str,
+    help = 'provide prefix'
+    )
+parser.add_argument(
+    '--minlen',
+    type = int,
+    help = 'Provide minimal file name.'
+    )
+parser.add_argument(
+    '--maxlen',
+    type = int,
+    help = 'Provide maximal file name. Must be greater or equal than prefix'
+    )
+
+args = parser.parse_args()
+prefix = args.prefix
+min_name_len = args.minlen
+max_name_len = args.maxlen
 dir_list = os.listdir(".")
 files_to_delete_count = 0
 
+if min_name_len - len(prefix) < 1:
+    min_name_len = 1
 if len(prefix) > int(max_name_len):
     sys.exit("ERROR: The prefix if bigger than acceptable file name length")
+
 for _file in dir_list:
-    match = re.fullmatch("{pr}.{{{mn},{mx}}}".format(pr = prefix, mn = int(min_name_len) - len(prefix), mx = int(max_name_len) - len(prefix)), _file)
+    match = re.fullmatch("{pr}.{{{mn},{mx}}}".format(pr = prefix, mn = min_name_len-len(prefix), mx = max_name_len-len(prefix)), _file)
     if match == None:
         continue
     os.remove(match.group())
     files_to_delete_count += 1
-print("Deleted " + str(files_to_delete_count) + " files.\n")
+print("Deleted " + str(files_to_delete_count) + " files.")
